f13cd34c57d4f94d6713fb8ba400da5ab2b1084c
regiprnm432 <regi.purnama.tif421@polban.ac.id>
2024-04-21 03:29:01 +0700

    update calculator app

---
+++ gradle_test/app/src/main/java/org/example/Computation.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Computation {
+    public static double compute(double angka1, double angka2, char operator) {
+        double result = 0;
+        switch (operator) {
+            case '1':
+                result = Operator.tambah(angka1, angka2);
+                break;
+            case '2':
+                result = Operator.kurang(angka1, angka2);
+                break;
+            case '3':
+                result = Operator.kali(angka1, angka2);
+                break;
+            case '4':
+                result = Operator.bagi(angka1, angka2);
+                break;
+            default:
+                throw new IllegalArgumentException("Operator tidak valid: " + operator);
+        }
+        return result;
+    }
+}
--- gradle_test/app/src/main/java/org/example/Validator.java
+++ gradle_test/app/src/main/java/org/example/Validator.java
@@ -21,4 +21,28 @@ public class Validator {
     public static boolean isValidDivision(double angka2) {
         return angka2 != 0;
     }
+
+    public static void validateInput(String input) {
+        if (!isNumeric(input)) {
+            System.out.println("Error: Input harus berupa angka");
+            System.exit(0);
+        }
+        double angka = Double.parseDouble(input);
+        if (!isInRange((int) angka)) {
+            System.out.println("Error: Angka harus berada dalam range -32,768 hingga 32,767");
+            System.exit(0);
+        }
+    }
+
+    public static void validateOperator(char operator, double angka2) {
+        if (!isValidOperator(operator)) {
+            System.out.println("Error: Operator tidak valid");
+            System.exit(0);
+        }
+        if (operator == '4' && !isValidDivision(angka2)) {
+            System.out.println("Error: Tidak dapat melakukan pembagian dengan nol");
+            System.exit(0);
+        }
+    }
+
 }
