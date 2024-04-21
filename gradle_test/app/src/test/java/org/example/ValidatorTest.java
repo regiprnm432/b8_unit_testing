d6774da1ace65922767c6feaa5444a67da880002
regiprnm432 <regi.purnama.tif421@polban.ac.id>
2024-04-21 20:07:07 +0700

    update test script

--- gradle_test/app/src/test/java/org/example/ValidatorTest.java
+++ gradle_test/app/src/test/java/org/example/ValidatorTest.java
@@ -8,49 +8,47 @@ public class ValidatorTest {
     @Test
     public void testIsNumeric() {
         assertTrue(Validator.isNumeric("123"));
-        assertTrue(Validator.isNumeric("-456"));
         assertFalse(Validator.isNumeric("abc"));
-        assertTrue(Validator.isNumeric("12.34"));
         System.out.println("Test case: testIsNumeric passed");
     }
 
     @Test
     public void testIsInRange() {
         assertTrue(Validator.isInRange(100));
-        assertTrue(Validator.isInRange(-200));
-        assertFalse(Validator.isInRange(40000));
-        assertFalse(Validator.isInRange(-40000));
         System.out.println("Test case: testIsInRange passed");
     }
 
     @Test
     public void testIsValidOperator() {
         assertTrue(Validator.isValidOperator('1'));
-        assertTrue(Validator.isValidOperator('4'));
-        assertFalse(Validator.isValidOperator('5'));
-        assertFalse(Validator.isValidOperator('a'));
         System.out.println("Test case: testIsValidOperator passed");
     }
 
     @Test
     public void testIsValidDivision() {
         assertTrue(Validator.isValidDivision(5));
-        assertFalse(Validator.isValidDivision(0));
         System.out.println("Test case: testIsValidDivision passed");
     }
 
     @Test
     public void testValidateInput_NonNumeric() {
         assertThrows(IllegalArgumentException.class, () -> {
-            Validator.validateInput("abc");
+            Validator.validateInput("s");
         });
         System.out.println("Test case: testValidateInput_NonNumeric passed");
     }
 
     @Test
-    public void testValidateInput_OutOfRange() {
+    public void testValidateInput_OutOfRangePlus() {
         assertThrows(IllegalArgumentException.class, () -> {
-            Validator.validateInput("40000000000000");
+            Validator.validateInput("1000000");
+        });
+        System.out.println("Test case: testValidateInput_OutOfRange passed");
+    }
+
+    public void testValidateInput_OutOfRangeMinus() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Validator.validateInput("-1000000");
         });
         System.out.println("Test case: testValidateInput_OutOfRange passed");
     }
@@ -58,7 +56,14 @@ public class ValidatorTest {
     @Test
     public void testValidateOperator_InvalidOperator() {
         assertThrows(IllegalArgumentException.class, () -> {
-            Validator.validateOperator('5', 10);
+            Validator.validateOperator('8', 10);
+        });
+        System.out.println("Test case: testValidateOperator_InvalidOperator passed");
+    }
+
+    public void testValidateOperator_validOperator() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Validator.validateOperator('1', 10);
         });
         System.out.println("Test case: testValidateOperator_InvalidOperator passed");
     }
@@ -69,5 +74,12 @@ public class ValidatorTest {
             Validator.validateOperator('4', 0);
         });
         System.out.println("Test case: testValidateOperator_DivisionByZero passed");
+    }
+
+    public void testValidateOperator_DivisionByTWo() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Validator.validateOperator('4', 2);
+        });
+        System.out.println("Test case: testValidateOperator_DivisionByTwo passed");
     }
 }
